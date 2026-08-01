c0a252cf9d77de8a4882c545945540b23c31311c
FarhanAzrayl <sam_farhan94@yahoo.com>
2026-08-02 04:53:15 +0800

    Testing - Just added the deployment details in the API Gateway

--- terraform/modules/apigateway/deployment.tf
+++ terraform/modules/apigateway/deployment.tf
@@ -1,1 +1,23 @@
 # Creates Deployment, Stage - Basically saying "Yo, publish/send out this API"
+
+# Connects the HTTP API to the Lambda function.
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id = aws_apigatewayv2_api.http.id
+  integration_type = "AWS_PROXY"
+  integration_uri = var.lambda_invoke_arn
+  integration_method = "POST"
+  payload_format_version = "2.0"
+}
+
+# Routing of the API Gateway, basically macam the reception bagi direction where to go
+resource "aws_apigatewayv2_route" "default" {
+
+  api_id = aws_apigatewayv2_api.http.id
+
+  # Matches every HTTP method and every path
+  # This will be based on Lambda's: event["requestContext"]["http"]["method"] and event["rawPath"] in the Zipfile handler
+  route_key = "$default"
+
+  # Okay so this tells the call that this route uses this integration which involves THIS Lambda. Route only knows the integration, they know nothing about our Lambda
+  target = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
